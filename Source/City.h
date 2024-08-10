4416c12acd5d25bcd7d84ec8a8ead6cb963fbc9e
tuandnm2912 <145586115+tuandnm2912@users.noreply.github.com>
2024-08-10 23:16:59 +0700

    Add files via upload

--- Source/City.h
+++ Source/City.h
@@ -1,9 +1,23 @@
 #include <utility>
 #include <string>
-
+#include <sstream>
 struct City{
     std::string name;
     std::pair<double, double> coordinate;
-    // std::string country;
-    // long population;
+    std::string country;
+    long population;
 };
+
+City extractInfo(std::string line){
+    std::stringstream ss(line);
+    City res;
+    char tmp;
+    std::getline(ss, res.name, ',');
+    ss >> res.coordinate.first;
+    ss >> tmp;
+    ss >> res.coordinate.second;
+    ss >> tmp;
+    std::getline(ss, res.country, ',');
+    ss >> res.population;
+    return res;
+}
