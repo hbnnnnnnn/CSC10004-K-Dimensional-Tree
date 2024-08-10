182c995e40dcba27d73fe889943e8b5a38ff6321
tuandnm2912 <145586115+tuandnm2912@users.noreply.github.com>
2024-08-10 23:50:26 +0700

    Add files via upload

---
+++ Source/City.cpp
@@ -0,0 +1,15 @@
+#include "City.h"
+
+City extractInfo(std::string line){
+    std::stringstream ss(line);
+    City res;
+    char tmp;
+    std::getline(ss, res.name, ',');
+    ss >> res.coordinate.first;
+    ss >> tmp;
+    ss >> res.coordinate.second;
+    ss >> tmp;
+    std::getline(ss, res.country, ',');
+    ss >> res.population;
+    return res;
+}
