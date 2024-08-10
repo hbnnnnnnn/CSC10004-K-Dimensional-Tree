117d854b13edd3790e90006979c8ee8b4c9312c8
Ha Bao Ngoc <hiembaonon@gmail.com>
2024-08-10 19:10:10 +0700

    fix KDTree.h convention

--- KDTree.h
+++ KDTree.h
@@ -14,15 +14,13 @@ struct KDNode {
 struct KDTree{
     KDNode* root;
     KDTree() : root(NULL){};
-
-    void insertKDNode(const City& newCity);
-
     ~KDTree();
+    void insertKDNode(const City& newCity);
     std::vector<City> rangeSearch(std::pair<double, double> bottomLeft, std::pair<double,double> topRight);
     City nearestNeighbour(std::pair<double, double> point);
 private:
-    KDNode* insertKDNodeRec (KDNode*& root, const City& newCity, int depth);
     void deleteTreeRec(KDNode* root);
-    void nearestNeighbourRec(City& res, KDNode* root, std::pair<double, double> point, int depth, double minDis);
+    KDNode* insertKDNodeRec (KDNode*& root, const City& newCity, int depth);
     void rangeSearchRec(KDNode* root, std::pair<double, double> bottomLeft, std::pair<double,double> topRight, int depth, std::vector<City>& res);
+    void nearestNeighbourRec(City& res, KDNode* root, std::pair<double, double> point, int depth, double minDis);
 };
