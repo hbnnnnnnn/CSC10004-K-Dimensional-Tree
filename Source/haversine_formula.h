8f016098658349639c6021d171c62fca489e637a
Ha Bao Ngoc <hiembaonon@gmail.com>
2024-08-30 21:12:27 +0700

    rewrote for c++ 11

--- Source/haversine_formula.h
+++ Source/haversine_formula.h
@@ -1,9 +1,8 @@
 #ifndef HARVERSINE_FORMULA_H_INCLUDED
 #define HARVERSINE_FORMULA_H_INCLUDED
 
-#include <numbers>
-#include <utility>
 #include <cmath>
+#include <utility>
 
 double calculate_distance(std::pair<double, double> pointA, std::pair<double, double> pointB);
 
